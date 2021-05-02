80a80840e2e8e5ab181aabc44b42f5a5fb325335
Leo Duran <29983534+lduran2@users.noreply.github.com>
2021-05-02 14:40:43 -0400

    support for new other school, charsets fixed

--- add-update-students.js
+++ add-update-students.js
@@ -2,11 +2,14 @@
 // Adds the PUT request to table 'update-students'.
 //
 // By        : Leomar Durán <https://github.com/lduran2/>
-// When      : 2021-05-02t02:36
+// When      : 2021-05-02t14:20
 // For       : ECE 5516
-// Version   : 1.0
+// Version   : 1.1
 //
 // Changelog :
+//	v1.1 - 2021-05-02t14:20
+//		supporting adding a new other school
+//
 //	v1.0 - 2021-05-02t02:36
 //		implemented addUpdateStudents
 //
@@ -16,8 +19,10 @@
 	const FORM = document.forms['update-students'];
 	// attach the event listener
 	FORM.addEventListener('submit', function (event) {
-		// lookup the school and #students
-		const SCHOOL = FORM['school'].value;
+		// resolve the school
+		const SCHOOL_SELECT = FORM['school'].value;
+		const SCHOOL = ('other'===SCHOOL_SELECT) ? FORM['other-school'].value : SCHOOL_SELECT;
+		// lookup # students
 		const N_STUDENTS = FORM['n_students'].value;
 		// create the JSON data object
 		const DATA = {
@@ -30,8 +35,9 @@
 		REQUEST.open('PUT', FORM.action, true);
 		REQUEST.setRequestHeader('Content-Type', 'application/json');
 		REQUEST.onload = function () {
-			alert(REQUEST.response);
-		}
+			console.log('request ( status', REQUEST.status, ') :');
+			console.log(REQUEST.response);
+		} /* REQUEST.onload */
 		REQUEST.send(JSON.stringify(DATA));
 
 		// prevent the GET submit
